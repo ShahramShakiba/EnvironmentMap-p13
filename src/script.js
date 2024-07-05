8229f699e8e207de8e6c7ea02216e75dd41b939a
ShahramShakiba <shahramshakiba88@gmail.com>
2024-07-05 17:42:23 +0330

    Initializing Environment Map Project

---
+++ src/script.js
@@ -0,0 +1,61 @@
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import GUI from 'lil-gui';
+import * as THREE from 'three';
+
+const canvas = document.querySelector('canvas.webgl');
+const gui = new GUI();
+const scene = new THREE.Scene();
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+//======================= Torus Knot =======================
+const torusKnot = new THREE.Mesh(
+  new THREE.TorusKnotGeometry(1, 0.4, 100, 16),
+  new THREE.MeshBasicMaterial()
+);
+torusKnot.position.y = 4;
+scene.add(torusKnot);
+
+//===================== Camera =========================
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
+camera.position.set(4, 5, 4);
+scene.add(camera);
+
+//================ Orbit Controls ======================
+
+const controls = new OrbitControls(camera, canvas);
+controls.target.y = 3.5;
+controls.enableDamping = true;
+
+//==================== Renderer ========================
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(width, height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+//================ Resize Listener ====================
+window.addEventListener('resize', () => {
+  width = window.innerWidth;
+  height = window.innerHeight;
+
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(width, height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+//=================== Animate =======================
+const clock = new THREE.Clock();
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+
+tick();
