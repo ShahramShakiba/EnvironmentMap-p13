d5b92a7ce1cad9fcd117acc88a199b8fdf8a7124
ShahramShakiba <shahramshakiba88@gmail.com>
2024-07-07 11:15:19 +0330

    Add the Model to the scene

--- src/script.js
+++ src/script.js
@@ -1,15 +1,17 @@
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import GUI from 'lil-gui';
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 
 const canvas = document.querySelector('canvas.webgl');
 const gui = new GUI();
 const scene = new THREE.Scene();
+const gltfLoader = new GLTFLoader();
 
 let width = window.innerWidth;
 let height = window.innerHeight;
 
-//======================= Torus Knot =======================
+//==================== Torus Knot ======================
 const torusKnot = new THREE.Mesh(
   new THREE.TorusKnotGeometry(1, 0.4, 100, 16),
   new THREE.MeshBasicMaterial()
@@ -17,13 +19,19 @@ const torusKnot = new THREE.Mesh(
 torusKnot.position.y = 4;
 scene.add(torusKnot);
 
+//====================== Models ========================
+gltfLoader.load('./models/FlightHelmet/glTF/FlightHelmet.gltf', (gltf) => {
+  console.log(gltf);
+  gltf.scene.scale.set(10, 10, 10);
+  scene.add(gltf.scene);
+});
+
 //===================== Camera =========================
 const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 100);
 camera.position.set(4, 5, 4);
 scene.add(camera);
 
 //================ Orbit Controls ======================
-
 const controls = new OrbitControls(camera, canvas);
 controls.target.y = 3.5;
 controls.enableDamping = true;
